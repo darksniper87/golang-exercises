468234f0c99f600db8038248734d07e396b379fb
Thrasos Thrasyvoulou <darksniper87@gmail.com>
2019-06-13 20:49:19 +0300

    fix comments

--- exercism/go/two-fer/two_fer.go
+++ exercism/go/two-fer/two_fer.go
@@ -1,9 +1,11 @@
+// Package twofer provides the implementation of 'Two Fer' exercise.
 package twofer
 
 import (
 	"fmt"
 )
 
+// ShareWith returns the full Two-fer sentence depending on the given name.
 func ShareWith(name string) string {
 	if name == "" {
 		name = "you"
