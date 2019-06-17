4d7d1b03e78059b983efa65c10fc4bd51c0c9e02
Thrasos Thrasyvoulou <darksniper87@gmail.com>
2019-06-17 19:13:01 +0300

    clear run

--- run.go
+++ run.go
@@ -5,26 +5,5 @@ import (
 )
 
 func main() {
-	test := []int{3, 6, 5, 8, 10, 20, 15}
-	fmt.Println(almostIncreasingSequence(test))
-}
-
-func almostIncreasingSequence(sequence []int) bool {
-	errorX, errorY := 0, 0
-
-	for i := 0; i < len(sequence)-2 && errorX < 2 && errorY < 2; i++ {
-		if sequence[i] >= sequence[i+1] {
-			errorX++
-		}
-
-		if sequence[i] >= sequence[i+2] {
-			errorY++
-		}
-	}
-
-	if sequence[len(sequence)-2] >= sequence[len(sequence)-1] {
-		errorX++
-	}
-
-	return errorX < 2 && errorY < 2
+	fmt.Println()
 }
